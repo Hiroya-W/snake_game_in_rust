e3e26009eba8296cfbab173541ab2af35e424270
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2019-12-31 14:09:53 +0900

    [add] ウィンドウが一瞬表示されるようになった

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+    let mut window: GlutinWindow = WindowSettings::new("Snake Game", [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 }
