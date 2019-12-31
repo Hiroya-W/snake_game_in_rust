ebdfe9cbb85600a9161c08b565324d41b6eead57
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2019-12-31 16:42:59 +0900

    [add] 常にウィンドウが表示されるようになった

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,22 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
+struct App {
+    gl: GlGraphics,
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics;
+
+        let GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        self.gl.draw(args.viewport(), |_c, gl| {
+            graphics::clear(GREEN, gl);
+        });
+    }
+}
+
 fn main() {
     let opengl = OpenGL::V3_2;
     let mut window: GlutinWindow = WindowSettings::new("Snake Game", [200, 200])
@@ -11,4 +27,15 @@ fn main() {
         .exit_on_esc(true)
         .build()
         .unwrap();
+
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+    }
 }
